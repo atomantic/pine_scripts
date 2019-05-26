41bd1f880f3311d40c161e1d9adda5d47040edeb
Adam Eivy <atomantic@gmail.com>
2019-05-26 08:10:01 -0700

    back to working

--- gen.dcacb.sh
+++ gen.dcacb.sh
@@ -25,7 +25,7 @@ milli_interval = 1000 * 60 * 60 * 24 * interval
 dollars = 1000" > dcacb.pine
 
 linewidth=8
-for period in 2190 1460 730 365 180 120 90 60 30
+for period in 2190 1825 1460 1095 730 365 180 120 90 60 30
 do
     color_string=$(echo "$color" | sed 's/0x/#/')
     echo -n "
@@ -33,15 +33,17 @@ do
 milli_${period} = milli_interval * ${period} 
 within_${period} = time_delta < milli_${period}
 total_${period} = 0.0
-spent_${period} = ${period} * dollars
+spent_${period} = 0
+quant_${period} = dollars/price // how many fractions/units bought this period
 if rolling_window
     spent_${period} := within_${period} ? nz(spent_${period}[1])+dollars : 0
     quant_${period} = within_${period} ? dollars/price : 0.0
     total_${period} := nz(total_${period}[1])+quant_${period}
 else
-    quant_${period} = dollars/price // how many fractions/units bought this period
     for i = 1 to ${period}
-        total_${period} := total_${period}+quant_${period}[i]
+        if quant_${period}[i]
+            spent_${period} := spent_${period}+dollars
+        total_${period} := total_${period}+nz(quant_${period}[i], 0)
 basis_${period} = spent_${period}/total_${period}
 plot_${period} = plot(basis_${period}, linewidth=${linewidth}, color=${color_string}, title=\"${period} days of DCA\")
 fill(plot1=plot_${period}, plot2=plot_price, color=price > basis_${period} ? color_fill_sell : color_fill_buy, transp=color_fill_transp)
